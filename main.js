d36a8ddcdbb311d343726e0dc795fcf5932c9d54
JesacaLin <JesacaLin@gmail.com>
2022-06-06 09:08:31 -0500

    added cors

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const people = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://motivational-quotes-api.herokuapp.com/api/${people}`
+    );
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("h2").innerText = data.quote;
+  } catch (error) {
+    console.log(error);
+  }
+}
